ba8b61429fef48b9009ef0b90267e1ac992b3bce
Brian C Ahaneku <72224090+brianahaneku@users.noreply.github.com>
2023-10-15 23:01:15 -0400

    Time: 32 ms (98.45%), Space: 16.7 MB (35.2%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -2,7 +2,7 @@ class Solution:
     def hIndex(self, citations: List[int]) -> int:
         n = len(citations)
         citations.sort()
-        hIndex = 1 if citations[-1] > 0 else 0
+        hIndex = 0
         for ix, x in enumerate(citations):
             hIndex = max(hIndex, min(x, n - ix))
         return hIndex
